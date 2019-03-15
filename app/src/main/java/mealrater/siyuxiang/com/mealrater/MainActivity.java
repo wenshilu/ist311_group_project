7bf20babc3b5a8da427c352f56dfa164137a364e
Siyu Xiang <siyuxiang@SiyudeMacBook-Air.local>
2019-03-14 19:14:01 -0700

    initialize group project

---
+++ app/src/main/java/mealrater/siyuxiang/com/mealrater/MainActivity.java
@@ -0,0 +1,13 @@
+package mealrater.siyuxiang.com.mealrater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
